4a149a2b1bb3bf3a441745b3918029a0fd2833db
Idris Raja <idris.raja@gmail.com>
2023-01-28 14:28:51 -0600

    chapter 1

--- 01/factorial.lua
+++ 01/factorial.lua
@@ -1,9 +1,13 @@
 function fact (n)
-    if n==0 then
+    if n<=0 then
         return 1
     else
         return n * fact(n-1)
     end
+end
+
+function twice (x)
+    return 2.0 * x
 end
 
 print("enter a number:")
--- shell.nix
+++ shell.nix
@@ -1,8 +1,13 @@
 with (import <nixpkgs> { });
 mkShell {
   buildInputs = [
-    lua
-    luajitPackages.lua-lsp
+    lua5_3
+    lua53Packages.lua-lsp
 
   ];
+
+  shellHook = ''
+    export LUA_PATH="$PWD/01/?.lua;$LUA_PATH"
+  '';
+
 }
